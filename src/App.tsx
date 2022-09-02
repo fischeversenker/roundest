23c1ad1019fa6f75e6bee2120e477ad276d66858
Felix Hamann <felix.hamann@satellytes.com>
2022-09-02 22:52:32 +0200

    feat: add debounced loading animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,46 @@
-import { Component, createResource, For, Show } from 'solid-js';
+import { debounce } from '@solid-primitives/scheduled';
+import { Component, createResource, createSignal, For, Show } from 'solid-js';
 import styles from './App.module.css';
 
 const fetchMatch = async (): Promise<
   { name: string; imageUrl: string; id: number }[]
 > => {
-  return new Promise((resolve) => {
-    const delay = Math.random() * 420 + 567;
-    setTimeout(
-      async () =>
-        resolve(
-          (await fetch('http://localhost:8080/match').then((res) =>
-            res.json()
-          )) as { name: string; imageUrl: string; id: number }[]
-        ),
-      delay
-    );
-  });
+  return await fetch('http://localhost:8080/').then((res) => res.json());
 };
 
 const App: Component = () => {
   const [matches, { refetch: refetchMatches }] = createResource(fetchMatch);
 
-  async function markAsRounder(id: number) {
-    await fetch('http://localhost:8080/vote', {
-      method: 'POST',
-      body: JSON.stringify({ for: id }),
-      headers: { ContentType: 'application/json' },
-    });
-    refetchMatches();
+  async function handleClick(id: number) {
+    setIsLoadingDebounced();
+
+    await fetch(`http://localhost:8080/vote/${id}`);
+    await refetchMatches();
+
+    setIsLoadingDebounced.clear();
+    setIsLoading(false);
   }
+
+  const [isLoading, setIsLoading] = createSignal(false);
+  const setIsLoadingDebounced = debounce(() => setIsLoading(true), 400);
 
   return (
     <div class={styles.App}>
-      <div class={styles.flex}>
-        <Show
-          when={!matches.loading}
-          fallback={<span class={styles['lds-dual-ring']}></span>}
-        >
-          <For each={matches()}>
-            {(match) => (
-              <div class={styles.match} onClick={() => markAsRounder(match.id)}>
-                <figure class={styles.figure}>
-                  <img src={match.imageUrl}></img>
-                </figure>
-                <p>{match.name}</p>
-              </div>
-            )}
-          </For>
-        </Show>
-      </div>
+      <Show
+        when={!isLoading()}
+        fallback={<span class={styles['lds-dual-ring']}></span>}
+      >
+        <For each={matches()}>
+          {(match) => (
+            <div class={styles.match} onClick={() => handleClick(match.id)}>
+              <figure class={styles.figure}>
+                <img src={match.imageUrl}></img>
+              </figure>
+              <p>{match.name}</p>
+            </div>
+          )}
+        </For>
+      </Show>
     </div>
   );
 };
